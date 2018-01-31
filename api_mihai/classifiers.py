dd7692ee21ba7603eeb4bb52915057356e4a17b4
Mihai <mihaivisuian@gmail.com>
2018-01-31 20:52:26 +0000

    PEP8 fixes

--- api_mihai/classifiers.py
+++ api_mihai/classifiers.py
@@ -18,19 +18,20 @@ class KmeansClassifier(object):
         # initialise means of queried columns
         means = np.zeros((cluster_no, len(cols)))
         # assign clustered_indices to dataframe
-        b = np.zeros((len(data), len(cols)+1))
-        b[:,:-1] = data.values_list(*cols)
-        b[:,-1] = indices
+        b = np.zeros((len(data), len(cols) + 1))
+        b[:, :-1] = data.values_list(*cols)
+        b[:, -1] = indices
         # compute means of queried columns
         for index in np.unique(indices):
-            means[index] = np.mean(b[b[:,-1]==index][:-1])
+            means[index] = np.mean(b[b[:, -1] == index][:-1])
         return means, indices
 
-
-    # method to get the clusters of the environments corresponding to the 5 environments based on
+    # method to get the clusters of the environments corresponding
+    # to the 5 environments based on
     # the queried columns and number of location clusters
     def get_environment_clusters(self, data, cluster_no, cols):
-        means, clustered_indices = self.__get_location_cluster_means(data, cluster_no, cols)
+        means, clustered_indices = self.__get_location_cluster_means(
+            data, cluster_no, cols)
         # we have 5 clusters corresponding to 5 different environments
         environment_kmeans = KMeans(n_clusters=5, random_state=0)
         # predict the cluster indices
--- api_mihai/views.py
+++ api_mihai/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 
 from .models import *
@@ -8,25 +7,29 @@ from .classifiers import KmeansClassifier
 
 # view for retrieving midday meadows walking data
 def labelled_midday_bins(request):
-	serializer = GeoJsonSerializer()
-	classifier = KmeansClassifier()
-	# remove bin0 outliers
-	bin_vals = ['bin'+str(x) for x in range(0,16)]
-	features = CollectedData.objects.order_by('time').filter(dataset__name="Walking Meadows Data Midday").filter(bin0__gt=0).filter(bin0__lt=450)
-	clusters = classifier.get_environment_clusters(features, 80, bin_vals)
-	return JsonResponse(
-		serializer.serialize(CollectedData, features, clusters)
-	)
+    serializer = GeoJsonSerializer()
+    classifier = KmeansClassifier()
+    # remove bin0 outliers
+    bin_vals = ['bin' + str(x) for x in range(0, 16)]
+    features = CollectedData.objects.order_by('time').filter(
+        dataset__name="Walking Meadows Data Midday").filter(
+        bin0__gt=0).filter(bin0__lt=450)
+    clusters = classifier.get_environment_clusters(features, 80, bin_vals)
+    return JsonResponse(
+        serializer.serialize(CollectedData, features, clusters)
+    )
 
 
 # view for retrieving afternoon meadows walking data
 def labelled_afternoon_bins(request):
-	serializer = GeoJsonSerializer()
-	classifier = KmeansClassifier()
-	# remove bin0 outliers
-	bin_vals = ['bin'+str(x) for x in range(0,16)]
-	features = CollectedData.objects.order_by('time').filter(dataset__name="Walking Meadows Data Afternoon").filter(bin0__gt=0).filter(bin0__lt=450)
-	clusters = classifier.get_environment_clusters(features, 80, bin_vals)
-	return JsonResponse(
-		serializer.serialize(CollectedData, features, clusters)
-	)
+    serializer = GeoJsonSerializer()
+    classifier = KmeansClassifier()
+    # remove bin0 outliers
+    bin_vals = ['bin' + str(x) for x in range(0, 16)]
+    features = CollectedData.objects.order_by('time').filter(
+        dataset__name="Walking Meadows Data Afternoon").filter(
+        bin0__gt=0).filter(bin0__lt=450)
+    clusters = classifier.get_environment_clusters(features, 80, bin_vals)
+    return JsonResponse(
+        serializer.serialize(CollectedData, features, clusters)
+    )
